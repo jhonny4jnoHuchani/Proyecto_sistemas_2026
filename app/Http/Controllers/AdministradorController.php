b0ef1a1199e66797a3fa5e1295e4a3f458b854db
jhonny4jnoHuchani <hynoku2004r@gmail.com>
2026-06-09 14:36:11 -0400

    modelos declarados

---
+++ app/Http/Controllers/AdministradorController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\administrador;
+use Illuminate\Http\Request;
+
+class AdministradorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(administrador $administrador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(administrador $administrador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, administrador $administrador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(administrador $administrador)
+    {
+        //
+    }
+}
